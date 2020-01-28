e6da07a10801b188da366dbc55a05c91df7a0d0d
Jon Thysell (JAUNTY) <jthysell@microsoft.com>
2020-01-28 12:33:33 -0800

    Created windows stub for DualscreenDeviceinfo * Followed instructions at https://github.com/Microsoft/react-native-windows/blob/master/vnext/docs/NativeModulesSetup.md * Updated to RN60 : `yarn add react-native@0.60 --dev` * Added RNW : `yarn add react-native-windows@vnext --peer` * Created windows stub for DualScreenDeviceinfo module

---
+++ packages/dualscreen-deviceinfo/windows/DualscreenDeviceinfo/DualscreenDeviceinfo.h
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "NativeModules.h"
+
+namespace DualscreenDeviceinfo {
+
+REACT_MODULE(DualscreenDeviceinfo);
+struct DualscreenDeviceinfo {
+
+};
+
+}
