7d36218b92eef45c59be3d912160e345d5669d1d
Evgeny Utkin <evutkin@microsoft.com>
2020-06-02 19:43:26 +0200

    Migrating to dualscreen-layout lib

--- dualscreeninfo/android/src/main/kotlin/com/microsoft/reactnativedualscreen/dualscreen/DualScreenInfo.kt
+++ dualscreeninfo/android/src/main/kotlin/com/microsoft/reactnativedualscreen/dualscreen/DualScreenInfo.kt
@@ -1,90 +1,64 @@
 package com.microsoft.reactnativedualscreen.dualscreen
 
-import android.content.Context
 import android.graphics.Rect
-import android.view.View
-import android.view.WindowManager
-import com.facebook.react.bridge.*
+import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.Arguments.createMap
+import com.facebook.react.bridge.LifecycleEventListener
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.modules.core.DeviceEventManagerModule.RCTDeviceEventEmitter
-import com.microsoft.device.display.DisplayMask
-
+import com.microsoft.device.dualscreen.layout.ScreenHelper
+import com.microsoft.device.dualscreen.layout.ScreenModeListener
+import com.microsoft.device.dualscreen.layout.SurfaceDuoScreenManager
 
 const val HINGE_WIDTH_KEY = "hingeWidth"
 const val IS_DUALSCREEN_DEVICE_KEY = "isDualScreenDevice"
-const val FEATURE_NAME = "com.microsoft.device.display.displaymask"
 
-class DualScreenInfo constructor(context: ReactApplicationContext) : ReactContextBaseJavaModule(context), LifecycleEventListener  {
-	private val mDisplayMask: DisplayMask?
-		get() {
-			return if(currentActivity != null) DisplayMask.fromResourcesRect(currentActivity) else null
-		}
-	private val rotation: Int
-		get() {
-			val wm = currentActivity?.getSystemService(Context.WINDOW_SERVICE) as WindowManager?
-			return wm?.defaultDisplay?.rotation ?: 0
-		}
-	private val hinge: Rect
-		get() {
-			val boundings = mDisplayMask?.getBoundingRectsForRotation(rotation)
-			return if (boundings == null || boundings.size == 0) {
-				Rect(0, 0, 0, 0)
-			} else boundings[0]
-		}
+class DualScreenInfo constructor(context: ReactApplicationContext) :
+	ReactContextBaseJavaModule(context), LifecycleEventListener, ScreenModeListener {
+	private val surfaceDuoScreenManager: SurfaceDuoScreenManager?
+		get() = if (currentActivity != null) SurfaceDuoScreenManager.init(currentActivity!!.application) else null
 	private val windowRects: List<Rect>?
-		get() = mDisplayMask?.getBoundingRectsForRotation(rotation)
-	private val windowRect: Rect
-		get() {
-			val windowRect = Rect()
-			val rootView: View? = currentActivity?.window?.decorView?.rootView
-			rootView?.getDrawingRect(windowRect)
-			return windowRect
-		}
-	private var mIsSpanning: Boolean = false
+		get() = if (currentActivity != null) ScreenHelper.getScreenRectangles(currentActivity!!) else null
+	private val isDualMode: Boolean
+			get() = if (currentActivity != null) ScreenHelper.isDualMode(currentActivity!!) else false
+	private var mIsSpanning = false
 
 	override fun getName() = "DualScreenInfo"
 
 	override fun initialize() {
 		super.initialize()
 		reactApplicationContext.addLifecycleEventListener(this)
-
-		val rootView: View? = currentActivity?.window?.decorView?.rootView
-		rootView?.addOnLayoutChangeListener { v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
-			emitUpdateStateEvent()
-		}
+		surfaceDuoScreenManager?.addScreenModeListener(this)
 	}
 
 	override fun getConstants(): Map<String, Any>? {
-        val constants: MutableMap<String, Any> = HashMap()
-    	constants[HINGE_WIDTH_KEY] = 34
-		constants[IS_DUALSCREEN_DEVICE_KEY] = reactApplicationContext.packageManager.hasSystemFeature(FEATURE_NAME)
+		val constants: MutableMap<String, Any> = HashMap()
+		constants[HINGE_WIDTH_KEY] = 34
+		constants[IS_DUALSCREEN_DEVICE_KEY] = if (currentActivity != null) ScreenHelper.isDeviceSurfaceDuo(currentActivity!!) else false
 
-    	return constants
-    }
+		return constants
+	}
 
 	override fun onHostResume() {
-		emitUpdateStateEvent()
+		emitUpdateStateEvent(isDualMode)
 	}
 
 	override fun onHostPause() {}
 
 	override fun onHostDestroy() {}
 
-	/**
-	 * Resolving a promise detecting if device is in Dual modes
-	 */
-	private fun isSpanning(): Boolean {
-		if (windowRect.width() > 0 && windowRect.height() > 0) {
-			return hinge.intersect(windowRect)
-		}
-
-		return false
+	override fun onSwitchToDualScreenMode() {
+		emitUpdateStateEvent(true)
 	}
 
-	private fun emitUpdateStateEvent() {
+	override fun onSwitchToSingleScreenMode() {
+		emitUpdateStateEvent(false)
+	}
+
+	private fun emitUpdateStateEvent(isSpanning: Boolean) {
 		if (reactApplicationContext.hasActiveCatalystInstance()) {
 			// Don't emit an event to JS if the dimensions haven't changed
-			val isSpanning = isSpanning()
 			if (mIsSpanning != isSpanning) {
 				mIsSpanning = isSpanning
 
@@ -103,8 +77,8 @@ class DualScreenInfo constructor(context: ReactApplicationContext) : ReactContex
 				params.putBoolean("isSpanning", isSpanning)
 				params.putArray("windowRects", windowRectsArray)
 				reactApplicationContext
-						.getJSModule(RCTDeviceEventEmitter::class.java)
-						.emit("didUpdateSpanning", params)
+					.getJSModule(RCTDeviceEventEmitter::class.java)
+					.emit("didUpdateSpanning", params)
 			}
 		}
 	}
