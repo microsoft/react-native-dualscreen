c2c54afa6c97a1cbdf86e46b7446d6d39ae7f4ce
Aaron Davida <adavi@microsoft.com>
2021-04-01 18:30:31 -0700

    remove unused imports

--- dualscreeninfo/android/src/main/kotlin/com/microsoft/reactnativedualscreen/dualscreen/DualScreenInfo.kt
+++ dualscreeninfo/android/src/main/kotlin/com/microsoft/reactnativedualscreen/dualscreen/DualScreenInfo.kt
@@ -1,16 +1,12 @@
 package com.microsoft.reactnativedualscreen.dualscreen
 
-import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
 import android.graphics.Rect
 import android.os.Build
-import android.util.DisplayMetrics
-import android.view.*
+import android.view.Surface
+import android.view.View
+import android.view.WindowManager
 import androidx.annotation.RequiresApi
-import androidx.core.view.WindowInsetsCompat
 import com.facebook.react.bridge.*
 import com.facebook.react.bridge.Arguments.createMap
 import com.facebook.react.modules.core.DeviceEventManagerModule.RCTDeviceEventEmitter
