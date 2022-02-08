4670420ea97c0016ecbd0591ceb8545192e4be9b
Nima Adibpour <niadibpo@microsoft.com>
2022-02-08 16:44:16 +0000

    Support both newer (R) and older (M) APIs to prevent crash on older Android OS versions "java.lang.NoSuchMethodError"

--- dualscreeninfo/android/src/main/kotlin/com/microsoft/reactnativedualscreen/dualscreen/DualScreenInfo.kt
+++ dualscreeninfo/android/src/main/kotlin/com/microsoft/reactnativedualscreen/dualscreen/DualScreenInfo.kt
@@ -36,24 +36,39 @@ class DualScreenInfo constructor(context: ReactApplicationContext) : ReactContex
 		}
 
 	private val mStatusBarHeight: Int
-		@RequiresApi(Build.VERSION_CODES.R)
+		@RequiresApi(Build.VERSION_CODES.M)
 		get() {
-			val stableInsetTop = currentActivity?.window?.decorView?.rootView?.rootWindowInsets?.getInsets(WindowInsets.Type.systemBars())?.top
-			return stableInsetTop ?: 0
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+				val stableInsetTop = currentActivity?.window?.decorView?.rootView?.rootWindowInsets?.getInsets(WindowInsets.Type.systemBars())?.top
+				return stableInsetTop ?: 0
+			} else {
+				val stableInsetTop = currentActivity?.window?.decorView?.rootView?.rootWindowInsets?.stableInsetTop
+				return stableInsetTop ?: 0
+			}
 		}
 
 	private val mBottomNavBarHeight: Int
-		@RequiresApi(Build.VERSION_CODES.R)
+		@RequiresApi(Build.VERSION_CODES.M)
 		get() {
-			val stableInsetBottom = currentActivity?.window?.decorView?.rootView?.rootWindowInsets?.getInsets(WindowInsets.Type.systemBars())?.bottom
-			return stableInsetBottom ?: 0
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+				val stableInsetBottom = currentActivity?.window?.decorView?.rootView?.rootWindowInsets?.getInsets(WindowInsets.Type.systemBars())?.bottom
+				return stableInsetBottom ?: 0
+			} else {
+				val stableInsetBottom = currentActivity?.window?.decorView?.rootView?.rootWindowInsets?.stableInsetBottom
+				return stableInsetBottom ?: 0
+			}
 		}
 
 	private val mSideNavBarHeight: Int
-		@RequiresApi(Build.VERSION_CODES.R)
+		@RequiresApi(Build.VERSION_CODES.M)
 		get() {
-			val stableInsetRight = currentActivity?.window?.decorView?.rootView?.rootWindowInsets?.getInsets(WindowInsets.Type.systemBars())?.right
-			return stableInsetRight ?: 0
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+				val stableInsetRight = currentActivity?.window?.decorView?.rootView?.rootWindowInsets?.getInsets(WindowInsets.Type.systemBars())?.right
+				return stableInsetRight ?: 0
+			} else {
+				val stableInsetRight = currentActivity?.window?.decorView?.rootView?.rootWindowInsets?.stableInsetRight
+				return stableInsetRight ?: 0
+			}
 		}
 
 	private val windowRects: List<Rect>
